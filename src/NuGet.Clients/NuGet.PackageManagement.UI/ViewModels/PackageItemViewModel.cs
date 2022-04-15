c4859adf92ace508a0aee39a17ce4eff35ec79e0
Fernando Aguilar <feaguila@microsoft.com>
2022-04-15 16:22:00 -0700

    Fixed unused metadata in PackageItemViewModel

--- src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/PackageItemViewModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/PackageItemViewModel.cs
@@ -698,7 +698,7 @@ namespace NuGet.PackageManagement.UI
                 }
                 else if (PackageLevel == PackageLevel.Transitive)
                 {
-                    await _searchService.GetPackageMetadataFromLocalSourcesAsync(identity, Project, Sources, cancellationToken);
+                    packageMetadata = await _searchService.GetPackageMetadataFromLocalSourcesAsync(identity, Project, Sources, cancellationToken);
                 }
 
                 PackageMetadata = packageMetadata;
